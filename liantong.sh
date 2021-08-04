18e8b2e176f5e9eba12ff664c4f912525af042b2
shiinarimo <58881591+shiinarimo@users.noreply.github.com>
2021-08-04 12:12:57 +0800

    Delete liantong.sh

--- liantong.sh
+++
@@ -1,7 +0,0 @@
-termux-setup-storage
-mkdir liantong
-cd liantong
-pkg update
-pkg install git  nodejs-lts wget curl 
-git clone https://github.com/simo8102/chinaunicom-AutoSignMachine.git .
-npm install
